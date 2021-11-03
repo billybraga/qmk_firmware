71129c4e8d48e48b6700b6b9f6c640ecfd117f59
Billy Braga <bbraga@progi.com>
2021-11-03 08:08:31 -0400

    updateBackup

--- waitforupdate.sh
+++ waitforupdate.sh
@@ -1,6 +1,13 @@
 #! /usr/bin/bash
 
 set -e
-while [ ! -f /mnt/c/Users/bbraga/Downloads/billybraga.json ]; do sleep 2; done
 
-$(dirname $0)/update.sh
+while [ 1=1 ]; do
+  echo WAITING
+  while [ ! -f /mnt/c/Users/bbraga/Downloads/billybraga.json ]; do sleep 2; done
+  $(dirname $0)/update.sh
+  if [ -f /mnt/c/Users/bbraga/Downloads/billybraga.json ]; then
+    echo "file wasn't deleted"
+    exit 1
+  fi
+done
