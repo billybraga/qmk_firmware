123b5e17f1786993e8da45602d63f2087e043801
Billy Braga <bbraga@progi.com>
2021-11-03 07:56:19 -0400

    updateBackup

--- update.sh
+++ update.sh
@@ -2,10 +2,11 @@
 set -x
 set -e
 cd ~/dev/thirdparty/qmk/qmk_firmware/
-rm -f ~/dev/thirdparty/qmk/qmk_firmware/backups/manuform_5x6.json
-mv /mnt/c/Users/bbraga/Downloads/billybraga.json ~/dev/thirdparty/qmk/qmk_firmware/backups/manuform_5x6.json
+rm -f backups/manuform_5x6.json
+mv /mnt/c/Users/bbraga/Downloads/billybraga.json backups/manuform_5x6.json
 git add .
 git commit -m updateBackup
-cd ~/dev/thirdparty/qmk/qmk_firmware/keyboards/handwired/dactyl_manuform/5x6/keymaps/retro
-qmk json2c ~/dev/thirdparty/qmk/qmk_firmware/backups/manuform_5x6.json > keymap.c
-qmk compile -j16 & git push
+qmk json2c backups/manuform_5x6.json > keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
+make --jobs=16 handwired/dactyl_manuform/5x6:retro
+qmk compile -j 16 & git push
+wait
