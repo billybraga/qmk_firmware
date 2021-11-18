47f10f496e9dc54456b80439bd24e6fe19d950dc
Billy Braga <bbraga@progi.com>
2021-11-18 08:20:45 -0500

    updateBackup

--- update.sh
+++ update.sh
@@ -6,6 +6,6 @@ rm -f backups/manuform_5x6.json
 mv /mnt/c/Users/bbraga/Downloads/billybraga.json backups/manuform_5x6.json
 git add .
 git commit -m updateBackup
-qmk json2c backups/manuform_5x6.json > keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
+qmk json2c backups/manuform_5x6.json > keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/retro_keymap.c
 make --jobs=16 handwired/dactyl_manuform/5x6:retro & git push
 wait
