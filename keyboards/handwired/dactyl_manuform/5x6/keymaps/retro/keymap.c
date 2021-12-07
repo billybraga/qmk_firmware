9200308874547ed9776165dd1fe3e77d801ff707
Billy Braga <bbraga@progi.com>
2021-12-07 12:43:09 -0500

    updateBackup

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
@@ -1,6 +1,10 @@
 #include QMK_KEYBOARD_H
 #include "retro_keymap.c"
 
+enum custom_keycodes {
+    MACRO_ARROW = RCS(KC_EQL),
+};
+
 bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LCTL_T(KC_NUBS):
@@ -9,3 +13,17 @@ bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
              return true;
     }
 }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case MACRO_ARROW:
+        if (record->event.pressed) {
+            // when keycode MACRO_ARROW is pressed
+            SEND_STRING("=>");
+        } else {
+            // when keycode MACRO_ARROW is released
+        }
+        break;
+    }
+    return true;
+};
