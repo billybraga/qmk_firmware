8fb2a9f12a43ca57188db986233745eda6ecfb6e
Billy Braga <bbraga@progi.com>
2021-12-16 08:26:09 -0500

    updateBackup

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
@@ -15,18 +15,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
     case KC_F17:
         if (record->event.pressed) {
-            // when keycode MACRO_ARROW is pressed
             SEND_STRING(" => ");
-        } else {
-            // when keycode MACRO_ARROW is released
         }
         break;
     case KC_F18:
         if (record->event.pressed) {
-            // when keycode MACRO_ARROW is pressed
             SEND_STRING("x => ");
-        } else {
-            // when keycode MACRO_ARROW is released
+        }
+        break;
+    case KC_F19:
+        if (record->event.pressed) {
+            SEND_STRING("`");
         }
         break;
     }
