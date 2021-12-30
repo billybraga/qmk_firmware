6d06dca7e09f79052ee856e07ba079a66854ae7d
Billy Braga <bbraga@progi.com>
2021-12-30 14:36:49 -0500

    updateBackup

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
@@ -94,6 +94,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 register_code(KC_LBRC);
                 unregister_code(KC_LBRC);
                 unregister_code(KC_RALT);
+                register_code(KC_SPC);
+                unregister_code(KC_SPC);
             }
             break;
         case TILDE:
