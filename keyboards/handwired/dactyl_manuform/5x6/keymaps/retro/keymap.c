31394e7d5af8c5fafdce4b19127375da6198c448
Billy Braga <bbraga@progi.com>
2022-06-11 14:00:14 -0400

    updateBackup

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
@@ -246,26 +246,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
 
         // layers
-        case R_THUMB:
-            if (record->event.pressed) {
-                layer_on(_R_THUMB);
-                update_tri_layer(_R_THUMB, _L_THUMB, _THUMBS);
-            } else {
-                layer_off(_R_THUMB);
-                update_tri_layer(_R_THUMB, _L_THUMB, _THUMBS);
-            }
-            return false;
-            break;
-        case L_THUMB:
-            if (record->event.pressed) {
-                layer_on(_L_THUMB);
-                update_tri_layer(_R_THUMB, _L_THUMB, _THUMBS);
-            } else {
-                layer_off(_L_THUMB);
-                update_tri_layer(_R_THUMB, _L_THUMB, _THUMBS);
-            }
-            return false;
-            break;
+        // case R_THUMB:
+        //     if (record->event.pressed) {
+        //         layer_on(_R_THUMB);
+        //         update_tri_layer(_R_THUMB, _L_THUMB, _THUMBS);
+        //     } else {
+        //         layer_off(_R_THUMB);
+        //         update_tri_layer(_R_THUMB, _L_THUMB, _THUMBS);
+        //     }
+        //     return false;
+        //     break;
+        // case L_THUMB:
+        //     if (record->event.pressed) {
+        //         layer_on(_L_THUMB);
+        //         update_tri_layer(_R_THUMB, _L_THUMB, _THUMBS);
+        //     } else {
+        //         layer_off(_L_THUMB);
+        //         update_tri_layer(_R_THUMB, _L_THUMB, _THUMBS);
+        //     }
+        //     return false;
+        //     break;
     }
     return true;
-};
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+  state = update_tri_layer_state(state, _R_THUMB, _L_THUMB, _THUMBS);
+  return state;
+}
