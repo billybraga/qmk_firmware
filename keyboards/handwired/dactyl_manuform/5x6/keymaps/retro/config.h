65a7ddef6a7f3fe216a268babe340157591005c1
Billy Braga <bbraga@progi.com>
2022-06-14 11:56:18 -0400

    updateBackup

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/config.h
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 #define MASTER_LEFT
 #define TAPPING_TOGGLE 2
+#define RETRO_TAPPING
 #define RETRO_TAPPING_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS
 #define UNICODE_SELECTED_MODES UC_WINC
