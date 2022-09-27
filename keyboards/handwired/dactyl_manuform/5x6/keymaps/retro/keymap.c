a6d63b0a01c955336989f26612441f6dcb66651e
Billy Braga <bbraga@progi.com>
2022-09-27 12:56:31 -0400

    updateBackup

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/retro/keymap.c
@@ -334,7 +334,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-uint32_t layer_state_set_user(uint32_t state) {
+layer_state_t layer_state_set_user(layer_state_t state) {
   state = update_tri_layer_state(state, _R_THUM_2_4, _L_THUM_3_4, _THUMBS);
   state = update_tri_layer_state(state, _R_THUM_2_4, _BEPO_SFT, _SFT_ARROWS);
   return state;
